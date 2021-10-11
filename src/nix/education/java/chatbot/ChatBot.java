d1e5704793830c9c604226ea06067e5e3747ba68
elCassador <c.illia@student.khai.edu>
2021-10-11 17:27:03 +0300

    ChatBot 1-st stage

---
+++ src/nix/education/java/chatbot/ChatBot.java
@@ -0,0 +1,7 @@
+package nix.education.java.chatbot;
+
+public class ChatBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is TEST.\nI was created in 2021.");
+    }
+}
