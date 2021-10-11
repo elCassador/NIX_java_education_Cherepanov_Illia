33ebcd7c344c3ac41affddfbc800a6dcd0666e9b
elCassador <c.illia@student.khai.edu>
2021-10-11 17:31:15 +0300

    ChatBot 2-nd stage

--- src/nix/education/java/chatbot/ChatBot.java
+++ src/nix/education/java/chatbot/ChatBot.java
@@ -1,7 +1,11 @@
 package nix.education.java.chatbot;
+import java.util.Scanner;
 
 public class ChatBot {
     public static void main(String[] args) {
         System.out.println("Hello! My name is TEST.\nI was created in 2021.");
+        System.out.println("Please, remind me your name.");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What a great name you have, " + scanner.nextLine() + "!");
     }
 }
