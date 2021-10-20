e7b0d9d0602ca7d5873b2eb6c78efb0758739449
elCassador <c.illia@student.khai.edu>
2021-10-20 08:48:45 +0300

    Coffee Machiine Project - Stage 1

--- src/nix/education/java/coffeemachine/CoffeeMachine.java
+++ src/nix/education/java/coffeemachine/CoffeeMachine.java
@@ -1,4 +1,19 @@
 package nix.education.java.coffeemachine;
 
 public class CoffeeMachine {
+
+
+    public static void main(String[] args) {
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+        coffeeMachine.makeCoffee();
+    }
+
+    public void makeCoffee() {
+        System.out.println("Starting to make a coffee\n" +
+                "Grinding coffee beans\n" +
+                "Boiling water\n" +
+                "Mixing boiled water with crushed coffee beans\n" +
+                "Pouring coffee into the cup\n" +
+                "Pouring some milk into the cup Coffee is ready!");
+    }
 }
