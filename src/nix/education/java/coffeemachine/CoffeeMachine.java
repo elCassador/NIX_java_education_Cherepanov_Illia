011b5b01be3f055686e29c7237cd5a497be1f0ab
elCassador <c.illia@student.khai.edu>
2021-10-20 11:43:33 +0300

    Coffee Machine project - Stage 6

--- src/nix/education/java/coffeemachine/CoffeeMachine.java
+++ src/nix/education/java/coffeemachine/CoffeeMachine.java
@@ -3,75 +3,27 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
 
-    Scanner scanner = new Scanner(System.in);
-//    private int oneEspressoWater = 250;
-//    private int oneEspressoMilk = 0;
-//    private int oneEspressoBeans = 16;
-//    private int oneEspressoCost = 4;
-//    private int oneLatteWater = 350;
-//    private int oneLatteMilk = 75;
-//    private int oneLatteBeans = 20;
-//    private int oneLatteCost = 7;
-//    private int oneCappuccinoWater = 200;
-//    private int oneCappuccinoMilk = 100;
-//    private int oneCappuccinoBeans = 12;
-//    private int oneCappuccinoCost = 6;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        MyClass coffeeMachine = new MyClass();
 
+        while (true) {
+            System.out.println("Write action (buy, fill, take, remaining, exit):");
+            coffeeMachine.input(scanner.nextLine());
+        }
+    }
+
+}
+
+class MyClass {
+
+    Scanner scanner = new Scanner(System.in);
     private int availableCoffeeWater = 400;
     private int availableCoffeeMilk = 540;
     private int availableCoffeeBeans = 120;
     private int availableCoffeeCups = 9;
     private int availableMoney = 550;
 
-    public static void main(String[] args) {
-        CoffeeMachine coffeeMachine = new CoffeeMachine();
-//        coffeeMachine.makeCoffee();
-//        coffeeMachine.howManyCups();
-//        coffeeMachine.makeCoffeeCups();
-        coffeeMachine.supplies();
-        while (true) {
-            coffeeMachine.input();
-        }
-    }
-
-    public void makeCoffee() {
-        System.out.println("Starting to make a coffee\n" +
-                "Grinding coffee beans\n" +
-                "Boiling water\n" +
-                "Mixing boiled water with crushed coffee beans\n" +
-                "Pouring coffee into the cup\n" +
-                "Pouring some milk into the cup Coffee is ready!");
-    }
-
-//    public void howManyCups() {
-//        System.out.println("Write how many cups of coffee you will need: ");
-//        int coffeeCups = scanner.nextInt();
-//        System.out.println("For " + coffeeCups + " cups of coffee you will need:\n" +
-//                coffeeCups * oneCoffeeWater + " ml of water\n" +
-//                coffeeCups * oneCoffeeMilk + " ml of milk\n" +
-//                coffeeCups * oneCoffeeBeans + " g of coffee beans");
-//    }
-//
-//    public void makeCoffeeCups() {
-//        System.out.println("Write how many ml of water the coffee machine has: ");
-//        availableCoffeeWater = scanner.nextInt();
-//        System.out.println("Write how many ml of milk the coffee machine has: ");
-//        availableCoffeeMilk = scanner.nextInt();
-//        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
-//        availableCoffeeBeans = scanner.nextInt();
-//        System.out.println("Write how many cups of coffee you will need: ");
-//        int coffeeCups = scanner.nextInt();
-//        int canMakeCups = Math.min(availableCoffeeWater/oneCoffeeWater, Math.min(availableCoffeeMilk/oneCoffeeMilk, availableCoffeeBeans/oneCoffeeBeans));
-//
-//        if (coffeeCups == canMakeCups) {
-//            System.out.println("Yes, I can make that amount of coffee");
-//        } else if (coffeeCups < canMakeCups) {
-//            System.out.println("Yes, I can make that amount of coffee (and even "
-//            + (canMakeCups - coffeeCups) + " more than that)");
-//        } else {
-//            System.out.println("No, I can make only " + canMakeCups + " cups of coffee");
-//        }
-//    }
 
     private void supplies() {
         System.out.println("The coffee machine has:\n" +
@@ -82,9 +34,7 @@ public class CoffeeMachine {
                 availableMoney + " of money\n");
     }
 
-    public void input() {
-        System.out.println("(buy, fill, take, remaining, exit):");
-        String option = scanner.nextLine();
+    public void input(String option) {
 
         switch (option) {
             case "buy":
@@ -105,7 +55,7 @@ public class CoffeeMachine {
         }
     }
 
-    public void coffeeTypes(int option) {
+    private void coffeeTypes(int option) {
         switch (option) {
             case 1:
                 availableMoney += 4;
@@ -194,7 +144,7 @@ public class CoffeeMachine {
         }
     }
 
-    public void fill() {
+    private void fill() {
         System.out.println("Write how many ml of water you want to add: ");
         availableCoffeeWater += scanner.nextInt();
         System.out.println("Write how many ml of milk you want to add: ");
@@ -205,7 +155,7 @@ public class CoffeeMachine {
         availableCoffeeCups += scanner.nextInt();
     }
 
-    public void take() {
+    private void take() {
         System.out.println("I gave you " + availableMoney);
         availableMoney = 0;
     }
