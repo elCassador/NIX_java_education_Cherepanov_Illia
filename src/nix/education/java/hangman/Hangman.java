281bc95a55665b9ad9ce86fe8d86a668c2161f39
elCassador <c.illia@student.khai.edu>
2021-10-28 16:42:01 +0300

    Hangman Project - Stage 3

--- src/nix/education/java/hangman/Hangman.java
+++ src/nix/education/java/hangman/Hangman.java
@@ -1,12 +1,15 @@
 package nix.education.java.hangman;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Hangman {
 
-    private String[] answers = new String[] {"java"};
-    private int answerIndex = 0;
-    private String userWord;
     private Scanner scanner = new Scanner(System.in);
+    private Random random = new Random();
+
+    private String[] answers = new String[] {"java", "python", "javascript", "kotlin"};
+    private int answerIndex = (int) (Math.random() * 4);
+    private String userWord;
 
     public void setWord() {
         System.out.println("Guess the word: > ");
